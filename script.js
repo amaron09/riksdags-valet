b34330337dc58aea62ca8236268122c028a290ab
DESKTOP-03CITA8\marcu <marcus.mh93@gmail.com>
2018-06-30 18:13:24 +0200

    Start project riksdagsval 2018

---
+++ script.js
@@ -0,0 +1,45 @@
+const parti = ["Socialdemokraterna", "Sverigedemokraterna", "Miljöpartiet", "Cenerpartiet", "Vänsterpartiet",
+ "Liberalerna", "Kristdemokraterna", "Utan partibeteckning", "Ny demokrati"];
+const valkrets = ["Blekinge län", "Dalarnas län", "Gotlands län", "Gävleborgs län", "Göteborgs län", "Hallands län",
+ "Jämtlands län", "Jönköpings län", "Kalmar län", "Kronobergs län", "Malmö län", "Norrbottens län", "Skåne läns norra och östra", 
+ "Skåne läns södra", "Skåne läns västra", "Stockholms kommun", "Södermanlands län", "Uppsala län", "Värmlands län", 
+ "Västerbottens län", "Västernorrlands län", "Västmanlands län", "Västra Götalands län norra", "Västra Götalands läns södra",
+ "Västra Götalands län västra", "Västra Götalands län östra", "Örebro län", "Östergötlands län"];
+
+createPartiSelector();
+
+async function searchLedamot(searchCriteria) {
+	const baseURL = "";
+	const responseObj = await fetch(baseURL + searchCriteria);
+	const matches = await responseObj.json();
+
+	console.log(matches);
+
+}
+
+function createCard(matches) {
+	const cardContainer = $("#cardContainer");
+	const randomPerson = Math.floor(Math.random() * 349);
+
+	
+}
+
+function createPartiSelector() {
+	const selectContainer = $("#partiFilter");
+	const valkretsContainer = $("#valkretsFilter");
+	console.log(selectContainer)
+	for (let i = 0; i < parti.length; i++) {
+		selectContainer.append(`<option value="${parti[i]}">${parti[i]}</option>`);
+		console.log("hej");
+	}
+
+	for (let i = 0; i < valkrets.length; i++) {
+		valkretsContainer.append(`<option value="${valkrets[i]}">${valkrets[i]}</option>`)
+	}
+	console.log("hej");
+}
+
+
+const url = "http://data.riksdagen.se/personlista/?iid=&fnamn=&enamn=&f_ar=&kn=&parti=&valkrets=&rdlstatus=&org=&utformat=json&termlista=";
+
+searchLedamot(url);
