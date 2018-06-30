a922dcb42943596b32ce3223f0cba6840ede6ff2
DESKTOP-03CITA8\marcu <marcus.mh93@gmail.com>
2018-06-30 22:10:05 +0200

    Better temp styling, added more info on ledamot

--- script/script.js
+++ script/script.js
@@ -5,6 +5,13 @@ const valkrets = ["Blekinge län", "Dalarnas län", "Gotlands län", "Gävleborg
  "Skåne läns södra", "Skåne läns västra", "Stockholms kommun", "Södermanlands län", "Uppsala län", "Värmlands län", 
  "Västerbottens län", "Västernorrlands län", "Västmanlands län", "Västra Götalands län norra", "Västra Götalands läns södra",
  "Västra Götalands län västra", "Västra Götalands län östra", "Örebro län", "Östergötlands län"];
+const utskott = ["Arbetsmarknadsutskottet", "Civilutskottet", "Finansutskottet", "Försvarsutskottet", "Justiteutskottet", 
+"Konstitutionsutskottet", "Kulturutskottet", "Miljö och jordbruksutskottet", "Näringsutskottet", "Skatteutskottet", 
+"Socialförsäkringsutskottet", "Socialutskottet", "Trafikutskottet", ""]
+
+
+const date = new Date();
+const currentYear = date.getFullYear();
 
 createPartiSelector();
 
@@ -25,21 +32,58 @@ function createCard(matches) {
 
 	cardContainer.append(`
 		<div class="ledamotCard row">
-			
-			<div class="col-3 pr-3">
+			<div class="col-md-12 col-lg-3 pr-3">
+				<h2>${personObject.tilltalsnamn} ${personObject.efternamn}</h2>
 				<img src="${personObject.bild_url_192}" alt="${personObject.sorteringsnamn} profilbild">
 			</div>
-			<div class="col-9">
+			<div class="col-md-12 col-lg-9">
+				<p id="candidate"></p>
 				<p><b>Parti: </b>${personObject.parti}</p>
+				<p><b>Ålder: </b>${currentYear-personObject.fodd_ar}</p>
 				<p><b>Status: </b>${personObject.status}</p>
 				<p><b>Valkrets: </b>${personObject.valkrets}</p>
-				<p><b>Utbildning: </b>${personObject.personuppgift.uppgift[2].uppgift}</p>
+				<p id="education"></p>
+				<p id="employments"></p>
+				
+				
+				<div class="row">
+					<div id="websiteLink" class="col-12">
+						
+					</div>
+				</div>
+
 			</div>
+
 			
 			
 
 		</div>
 		`);
+
+	const educationContainer = $("#education");
+	const websiteLinkButton = $("#websiteLink");
+	const candidateContainer = $("#candidate");
+	const employmentsContainer = $("#employments");
+
+	for (let uppgift of personObject.personuppgift.uppgift) {
+		if (uppgift.kod === "Utbildning") {
+			educationContainer.append(`<b>Utbildning: </b>${uppgift.uppgift}`)
+		}
+		if (uppgift.kod === "Webbsida") {
+			websiteLinkButton.append(`<a href="${uppgift.uppgift}" target="_blank">Läs mer</a>`)
+		}
+		if (uppgift.kod === "Anställningar") {
+			employmentsContainer.append(`<b>Anställningar: </b>${uppgift.uppgift}`)
+		}
+		if (uppgift.kod === "KandiderarINastaVal") {
+			if (uppgift.uppgift[0] === "true") {
+				candidateContainer.append(`<b>Kandiderar i nästa val: </b> Ja`)
+			}
+			else {
+				candidateContainer.append(`<b>Kandiderar i nästa val: </b> Nej`)
+			}
+		}
+	}
 
 	console.log("Hej");
 	console.log(matches.personlista.person[randomPerson]);
